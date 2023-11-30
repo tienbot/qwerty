5fd614ffb0b0c3550e7bd61c30d88941ed83852e
tienbot1 <tien.ru@mail.ru>
2023-11-30 16:27:07 +0300

    первый коммит

---
+++ script.js
@@ -0,0 +1,26 @@
+// 1 строка
+// 2 число
+// 3 большое число
+// 4 булево значение
+// 5 объект
+// 6 символ
+// 7 null
+// 8 undefined
+
+
+
+let x = 0.5;
+
+let person = {
+    age: 1,
+    name: 'Ivan',
+    male: 'man',
+    rus: true,
+}
+
+// console.log(test + ' ' + test2 + '!!!! ' + x + ' ' + y + '???');
+// console.log(`${test} ${test2}!!!! ${x} ${y}???`);
+
+console.log(typeof x)
+let y = 100;
+console.log(y)
